151d24de88806f751ad6f330147c16492e9693ce
Peter Dulacka <dulacka@gmail.com>
2011-06-17 12:27:12 +0200

    error message changed to be more specific

--- src/pd/fiit/gui/GUI.java
+++ src/pd/fiit/gui/GUI.java
@@ -404,7 +404,7 @@ public class GUI extends JFrame {
 					}
 					
 					if(fileListModel.get(0).equals("No video files in folder")){
-						JOptionPane.showMessageDialog(null, "Cannot find subtitles");
+						JOptionPane.showMessageDialog(null, "No video file selected, nothing to search for.");
 						return;
 					}
 
@@ -492,7 +492,7 @@ public class GUI extends JFrame {
 					return;
 				}
 				if(fileListModel.get(0).equals("No video files in folder")){
-					JOptionPane.showMessageDialog(null, "Cannot find subtitles");
+					JOptionPane.showMessageDialog(null, "No video file selected, nothing to search for.");
 					return;
 				}
 				initiateSearch();
